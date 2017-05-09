96edc9bc37ccb0375319178007dddca7f2ced2ec
Nicolas Peri <Nicolas Peri>
2017-05-08 23:22:11 -0300

    First Exercices

---
+++ chapter1/ej1/index.js
@@ -0,0 +1,5 @@
+// correr npm install
+var _ = require('underscore');
+_.each([1, 2, 3], function(v) {
+	console.log(v);
+})
---
+++ chapter1/homework/interface.js
@@ -0,0 +1,34 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  db.collection('movies').insert(doc, function(error, result) {
+		if(error) {
+			console.log(error);
+			process.exit(1);
+		}
+	});
+  callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+	var query = { director: director};
+  	db.collection('movies').find(query).sort({'title': 1}).toArray(function(error, docs) {
+		if(error) {
+			console.log(error);
+			process.exit(1);
+		}
+
+		// console.log('functionnd docs:');
+		// docs.forEach(function(doc) {
+		// 	console.log(JSON.stringify(doc));
+		// });
+		callback(null, docs);
+	});
+};
